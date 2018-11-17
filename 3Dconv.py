44222cf2e65d117714b87e37f9d3f5fe586ff4ca
futami <famichiki.yuuki@gmail.com>
2018-11-17 14:44:42 +0900

    update

--- 3Dconv.py
+++ 3Dconv.py
@@ -17,7 +17,7 @@ import model
 import tools
 
 def main(args):
-  
+
     """ setting """
     para_str = '3dconvNet_Epoch{}_Batchsize{}_SeqLength{}_Stride{}_dropout{}_loss{}_Adam'.format(
         args.epochs, args.batchsize, args.seqlength, args.strides, args.dropout, args.loss)
@@ -34,8 +34,8 @@ def main(args):
     
     """ load data """
     print("load csv files data")
-    #X_train, X_valid, Y_train, Y_valid = load.load_csv_data(args)
-    train_datagen = load.ImageDataGenerator(args)
+    X_train, X_valid, Y_train, Y_valid = load.load_csv_data(args)
+    #train_datagen = load.ImageDataGenerator(args)
 
     """ build model """
     conv3Dmodel = model.conv3D(args)
@@ -44,15 +44,20 @@ def main(args):
 
     conv3Dmodel.compile(loss=args.loss, optimizer=Adam())
 
-    """ start train """
+    """ start train 
     history = conv3Dmodel.fit_generator(
         generator=train_datagen.flow_from_directory(),
-        steps_per_epoch=3000,
+        steps_per_epoch= int(2000/ args.batchsize),
         epochs=args.epochs,
         callbacks=[tb_cb, reduce_lr],
         verbose=1
     )
-
+    """
+    conv3Dmodel.fit(X_train, Y_train,
+                    batch_size=args.batchsize,
+                    epochs=args.epochs,
+                    callbacks=[tb_cb, reduce_lr],
+                    validation_data=(X_valid, Y_valid))
     """ model save """
     if not os.path.exists('./saved_model/'):
         os.makedirs('./saved_model/')
@@ -66,10 +71,10 @@ if __name__ == '__main__':
     parser.add_argument('--datasetpath', '-p', type=str, required=False)
     parser.add_argument('--linetoken', '-t', type=str, required=False)
     parser.add_argument('--epochs', '-e', default=300)
-    parser.add_argument('--batchsize', '-b', default=32)
-    parser.add_argument('--strides', '-s', default=5)
-    parser.add_argument('--imgsize', '-i', default=128)
-    parser.add_argument('--seqlength', default=10)
+    parser.add_argument('--batchsize', '-b', default=16)
+    parser.add_argument('--strides', '-s', default=10)
+    parser.add_argument('--imgsize', '-i', default=64)
+    parser.add_argument('--seqlength', default=15)
     parser.add_argument('--dropout', default=0.3)
     parser.add_argument('--loss', '-l', type=str, default='binary_crossentropy')
 
